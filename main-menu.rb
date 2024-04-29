52b4cba9b7ecb8f28be185f2461cfcbc6f87fc55
GeovaneSFT <galefreitas@gmail.com>
2024-04-29 17:00:14 -0300

    feat: atualiza path consultas publicas

--- main-menu.rb
+++ main-menu.rb
@@ -15,6 +15,13 @@
       "is_visible"=> true
     },
     {
+      "id"=> "consultas",
+      "href"=> "/processes?filter%5Bwith_date%5D=active&filter%5Bwith_type%5D=1",
+      "label"=> "Consultas Públicas",
+      "sub_items"=> [],
+      "is_visible"=> true
+    },
+    {
       "id"=> "noticias",
       "href"=> "/processes/brasilparticipativo/f/26/posts",
       "label"=> "Notícias",
