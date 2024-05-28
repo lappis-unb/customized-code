e91428af79a7b00128e9f9932e6a1b470b71dda2
Eduardo Nunes <eduardonunes2525@gmail.com>
2024-05-28 13:25:05 -0300

    feat: atualiza RS

---
+++ compressor-html/html_compressor.py
@@ -0,0 +1,14 @@
+import re
+
+arquivo_entrada = 'index.html'
+arquivo_saida = 'newindex.html'
+
+with open(arquivo_entrada, 'r') as arquivo:
+    conteudo = arquivo.read()
+
+novo_conteudo = re.sub(r'(?<!https:)\/\/.*\n', '', conteudo)
+
+novo_conteudo = novo_conteudo.replace('\n', '')
+
+with open(arquivo_saida, 'w') as arquivo:
+    arquivo.write(novo_conteudo)
